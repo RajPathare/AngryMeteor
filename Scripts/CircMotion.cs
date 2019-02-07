303006875dc0f8b4e1137d737538ee78d82c303b
Raj Pathare <rajgpathare@gmail.com>
2019-02-07 19:48:15 +0530

    Add files via upload

---
+++ Scripts/CircMotion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircMotion : MonoBehaviour {
+
+
+	public float speed = 3f;
+
+	void Start () {
+
+    }
+   
+	
+	void FixedUpdate () {
+
+		if(Input.GetKey("a"))
+		{
+			transform.Rotate( 0f, 0f, speed);
+		}
+		if(Input.GetKey("d"))
+		{
+
+			transform.Rotate( 0f, 0f, -speed);
+		}
+        
+    }
+}
