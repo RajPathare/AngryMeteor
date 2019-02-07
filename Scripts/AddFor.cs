91c31cf5a7741f99ad0f1382c87bea48b0200a10
Raj Pathare <rajgpathare@gmail.com>
2019-02-07 19:47:26 +0530

    Add files via upload

---
+++ Scripts/AddFor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class AddFor : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		var pos = transform.position;
+
+		pos.y = pos.y + 0.03f;
+		transform.position = pos;
+
+		
+	}
+}
