32cf35adb948648ae6b61ef60307602da8f37cfc
Raj Pathare <rajgpathare@gmail.com>
2019-02-08 22:21:15 +0530

    Add files via upload

---
+++ Scripts/UIPresser.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIPresser : MonoBehaviour , IPointerDownHandler, IPointerUpHandler {
+
+	bool pressed = false;
+	public Transform shield;
+	public float speed = 3f;
+
+
+	public void OnPointerDown(PointerEventData eventData){
+
+		pressed = true;
+	}
+
+	public void OnPointerUp(PointerEventData eventData){
+
+		pressed = false;
+
+	}
+
+
+	void Update () {
+
+
+		if(pressed)
+		{
+			rotatee ();
+
+		}
+
+
+	}
+
+	public void rotatee(){
+
+		shield.Rotate(0f , 0f , -speed);
+	}
+}
